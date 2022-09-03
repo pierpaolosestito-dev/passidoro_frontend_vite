7f933e03567feaf53d83b76c3c000d5bda565692
pierpaolosestito200933 <sstppl99h29d122x@studenti.unical.it>
2022-09-03 21:27:57 +0200

    re-update
    re-update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-
+import adapter from '@sveltejs/adapter-netlify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  adapter:adapter(),
   plugins: [svelte()]
 })
